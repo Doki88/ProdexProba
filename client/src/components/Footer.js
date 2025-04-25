c1f274e17dc20d237aacc11ca6164c7980c30e0e
Dejan Prodanovic <dexpro988@gmail.com>
2025-04-25 15:12:51 +0200

    new post

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -70,20 +70,55 @@
 
 import React from 'react';
 import "../styles/footer.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'; // formerly faMapMarkerAlt
+
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <h2 className="footer-title">My Website Footer</h2>
+      <h2 className="footer-title"> PRODEX DOO</h2>
 
       <div className="footer-box-container">
-        <div className="footer-box">Box 1</div>
-        <div className="footer-box">Box 2</div>
-        <div className="footer-box">Box 3</div>
+        <div className="footer-box">
+        <div className="flex-container-two">
+            <FontAwesomeIcon icon={faLocationDot} style={{ color: 'white' }} /> 
+        </div>
+            <div className="flex-container-two-text">
+                <p>Adresa:<br></br>Cara Lazara 35, 74480 Modriča, BIH</p>
+
+            </div>
+        </div>
+        <div className="footer-box">
+                    <div className="flex-container-two">
+                    <FontAwesomeIcon icon={faPhone} style={{ color: 'white' }} />
+
+                    </div>
+                    <div className="flex-container-two-multi">
+                        <div className="flex-container-two-text1">
+                            <p>Tel:<br></br>053/820-891</p>
+                        </div>
+                        
+                        <div className="flex-container-two-text1">
+                            <p>Mob:<br></br>066-119-921</p>
+                        </div>
+                    </div>
+        </div>
+        <div className="footer-box">
+            <div className="flex-container-two">
+                <img src="/images/svg/reshot-icon-email-P9WA8LS724.svg"
+                    className="img-svg-email" alt="..."/>   
+            </div>
+            <div className="flex-container-two-text">
+                <p>Email:<br></br>prodexmd@yahoo.com</p> 
+            </div>
+        </div>
       </div>
 
       <div className="footer-info">
-        <p>© 2025 MyWebsite — All rights reserved. Here's some info about the site.</p>
+        <p>  All rights reserved @ {new Date().getFullYear()} Prodex DOO</p>
+        
       </div>
     </footer>
   );
